80997c5f3d6f21aef25cf56e33e2f23fd0e1f210
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2022-03-23 22:48:45 +0530

    completed Medium1 , added plot using plotly

--- Medium_Test_Solutions/Medium1/Plot.r
+++ Medium_Test_Solutions/Medium1/Plot.r
@@ -1,18 +1,46 @@
 library(readr)
 library(reshape2)
+library(plotly)
 
 time_series <- read_csv("time_series_covid19_confirmed_global.csv")
 
-#Renaming a column
-names(time_series)[names(time_series) == "Country/Region"] <-
-  "Country"
 
-#Remove unwanted columns
-time_series <- time_series[c(-1,-3,-4)]
-time_series <-
-  melt(
-    time_series,
-    id.vars = "Country",
-    value.name = "value",
-    variable.name = "date"
-  )
+
+
+create_plot <- function(country_name) {
+  # Renaming a column
+  names(time_series)[names(time_series) == "Country/Region"] <-
+    "Country"
+  time_series <- time_series[time_series$Country == country_name,]
+  # Remove unwanted columns
+  time_series <- time_series[c(-1, -3, -4)]
+  time_series <-
+    melt(
+      time_series,
+      id.vars = "Country",
+      value.name = "value",
+      variable.name = "date"
+    )
+
+  # Creating plot using plotly
+  fig <- plot_ly(time_series, type = "scatter", mode = "lines") %>%
+    add_trace(x =  ~ date,
+              y = ~ value,
+              name = country_name) %>%
+    layout(
+      showlegend = TRUE,
+      xaxis = list(
+        title = "Date",
+
+        tickformat = "%y",
+        tickangle = -90
+      ),
+      yaxis =  list(title = "Total Cases")
+    )
+  fig
+
+}
+
+
+
+create_plot("India")
